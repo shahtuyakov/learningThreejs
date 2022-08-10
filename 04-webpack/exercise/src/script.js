ac63d1d93e3cf2dc12df26e7a68f0b86f83c1f1f
shahtuyakov <49775161+shahtuyakov@users.noreply.github.com>
2022-08-09 20:06:21 -0400

    webpack2

--- 04-webpack/exercise/src/script.js
+++ 04-webpack/exercise/src/script.js
@@ -1,1 +1,33 @@
 import './style.css'
+import * as THREE from 'three';
+
+// scene
+const scene = new THREE.Scene();
+
+// red cube
+const cube = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({color: 'red'});
+const mesh = new THREE.Mesh(cube, material);
+scene.add(mesh);
+
+// sizes
+const sizes = {
+    width: 900,
+    height: 800
+}
+
+// camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 4;
+// camera.position.x = 3;
+// camera.position.y = 1;
+scene.add(camera);
+
+// renderer
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+});
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
