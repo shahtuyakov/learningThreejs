34390fea9b49123bab0d8248a30bb1bc92ed3173
Shokhzodjon Tuyokov <shokhzodjontuyokov@Shokhzodjons-MacBook-Pro.local>
2022-08-15 19:57:32 -0400

    07-cameras

--- 07-cameras/exercises/src/script.js
+++ 07-cameras/exercises/src/script.js
@@ -1,5 +1,19 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+// console.log(OrbitControls);
+/**
+ * Cursor
+ */
+const cursor = {
+    x: 0,
+    y: 0,
+}
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5;
+    cursor.y = -(event.clientY / sizes.height - 0.5);
+})
 
 /**
  * Base
@@ -21,16 +35,43 @@ const mesh = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
     new THREE.MeshBasicMaterial({ color: 0xff0000 })
 )
+// const mesh1 = new THREE.Mesh(
+//     new THREE.BoxGeometry(2, 1, 1, 5, 5, 5),
+//     new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+// )
 scene.add(mesh)
+// scene.add(mesh1)
 
-// Camera
-// const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
-const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 100)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+// PrespectiveCamera
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
+
+
+// OrthographicCamera 
+// const aspectRation = sizes.width / sizes.height;
+// const camera = new THREE.OrthographicCamera(
+//     -1 * aspectRation, 
+//     1 * aspectRation, 
+//     1, 
+//     -1, 
+//     0.1, 
+//     100
+// );
+
+
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 3;
 camera.lookAt(mesh.position)
 scene.add(camera)
+
+// Controls 
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+
+
+
+
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -46,7 +87,18 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    // mesh.rotation.y = elapsedTime;
+ 
+    // Update camera
+    // camera.position.x = cursor.x * 10;
+    // camera.position.y = cursor.y * 10;
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+    // camera.position.y = cursor.y * 5;
+    // camera.lookAt(mesh.position);
+
+    // Update Controls
+    controls.update();
 
     // Render
     renderer.render(scene, camera)
@@ -55,4 +107,4 @@ const tick = () =>
     window.requestAnimationFrame(tick)
 }
 
-tick()
+tick();
